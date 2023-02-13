3463566b5f5f8684bf81886cb7441a7a878be341
Ion Cerezo <ioncerezovalero@gmail.com>
2023-02-13 16:30:53 +0000

    create messages ejs template

--- routes/new.js
+++ routes/new.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET new page. */
 router.get('/', function(req, res, next) {
   res.send('newww');
 });
