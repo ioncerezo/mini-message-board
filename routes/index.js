1228c6c2fd0a7042d0f7d0183ef5276c8d78e826
Ion Cerezo <ioncerezovalero@gmail.com>
2023-02-13 16:49:26 +0000

    create basic post method

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,13 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.text, user: req.body.user, added: new Date()});
+  res.redirect('/new')
+});
+
 module.exports = router;
